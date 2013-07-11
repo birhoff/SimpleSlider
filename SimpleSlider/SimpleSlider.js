04a79675dd2b5b4d7f40177c92d7db24508633d3
birhoff <birhoff@gmail.com>
2013-07-11 10:29:00 +0300

    finish basic sliding functionality

--- SimpleSlider/SimpleSlider.js
+++ SimpleSlider/SimpleSlider.js
@@ -5,11 +5,15 @@
     $.fn.SimpleSlider = function (options) {
 
         // create settings from defaults and user options
-        var settings = $.extend({}, $.SimpleSlider.defaults, options);
+        var settings = $.extend({}, $.fn.SimpleSlider.defaults, options);
 
         var $wrapper = this;
         var $slider = $wrapper.find('ul');
         var $slides = $slider.children('li');
+
+        var $clone_first = null;
+        var $clone_last = null;
+        var $container = null;
 
         var state = {
             count: $slides.length,
@@ -38,7 +42,77 @@
                 'position': 'relative'
             });
 
+            if (settings.hoverpause && settings.automatic) {
+                $wrapper.hover(function () {
+                    if (!state.paused) {
+                        clearInterval(state.interval);
+                        state.paused = true;
+                    }
+                }, function () {
+                    if (state.paused) {
+                        state.interval = setInterval(function () {
+                            slide("fwd", false);
+                        }, settings.animdelay);
+                        state.paused = false;
+                    }
+                });
+            }
+
+            initSliding();
+
+            state.currentindex = 1;
+            state.currentslide = 2;
+
+            $slider.show();
+            $slides.eq(state.currentindex).show();
+
+            // Finally, if automatic is set to true, kick off the interval
+            if (settings.automatic) {
+                state.interval = setInterval(function () {
+                    slide("fwd", false);
+                }, settings.animdelay);
+            }
+
         }
+
+        var initSliding = function () {
+
+            $clone_first = $slides.eq(0).clone();
+            $clone_last = $slides.eq(state.count - 1).clone();
+
+            $clone_first.attr({'data-clone': 'last', 'data-slide': 0}).appendTo($slider).show();
+            $clone_last.attr({'data-clone': 'first', 'data-slide': 0}).prependTo($slider).show();
+
+            $slides = $slider.children('li');
+            state.count = $slides.length;
+
+            $container = $('<div class="simpleSlider-wrapper"></div>');
+
+
+            $container.css({
+                'width': settings.width,
+                'height': settings.height,
+                'overflow': 'hidden',
+                'position': 'relative'
+            });
+
+            $slider.addClass("SimpleSlider");
+
+            $slider.css({
+                'width': settings.width * (state.count + 2),
+                'left': -settings.width * state.currentSlide
+            });
+
+            $slides.css({
+                'float': 'left',
+                'position': 'relative',
+                'display': 'list-item'
+            });
+
+            $container.prependTo($wrapper);
+            $slider.appendTo($container);
+
+        };
 
         var readOptions = function (wrapper, options) {
 
@@ -51,10 +125,85 @@
             }
         }
 
+        var slide = function (direction, position) {
+
+            if (state.animating)return;
+
+            state.animating = true;
+
+            if (position) {
+                state.nextslide = position;
+                state.nextindex = position - 1;
+            }
+            else {
+                setNextSlide(direction);
+            }
+
+            state.slidewidth = settings.width;
+
+            $slider.animate({'left': -state.nextindex * state.slidewidth }, settings.animduration, function () {
+
+                state.currentslide = state.nextslide;
+                state.currentindex = state.nextindex;
+
+                // is the current slide a clone?
+                if ($slides.eq(state.currentindex).attr('data-clone') === 'last') {
+
+                    // affirmative, at the last slide (clone of first)
+                    $slider.css({'left': -state.slidewidth });
+                    state.currentslide = 2;
+                    state.currentindex = 1;
+
+                }
+                else if ($slides.eq(state.currentindex).attr('data-clone') === 'first') {
+
+                    // affirmative, at the fist slide (clone of last)
+                    $slider.css({'left': -state.slidewidth * (state.count - 2)});
+                    state.currentslide = state.count - 1;
+                    state.currentindex = state.count - 2;
+
+                }
+
+                state.animating = false;
+
+            });
+        };
+
+        var setNextSlide = function (direction) {
+
+            if (direction === "fwd") {
+
+                if ($slides.eq(state.currentindex).next().length) {
+                    state.nextindex = state.currentindex + 1;
+                    state.nextslide = state.currentslide + 1;
+                }
+                else {
+                    state.nextindex = 0;
+                    state.nextslide = 1;
+                }
+
+            }
+            else {
+
+                if ($slides.eq(state.currentindex).prev().length) {
+                    state.nextindex = state.currentindex - 1;
+                    state.nextslide = state.currentslide - 1;
+                }
+                else {
+                    state.nextindex = state.count - 1;
+                    state.nextslide = state.count;
+                }
+
+            }
+
+        };
+
+        init();
+
         return this;
     };
 
-    $.SimpleSlider.defaults = {
+    $.fn.SimpleSlider.defaults = {
         // w + h
         width: 900,
         height: 500,
